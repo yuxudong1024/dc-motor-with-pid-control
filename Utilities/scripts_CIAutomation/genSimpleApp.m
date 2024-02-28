601eb3758d0419f144d5108f266bdd6829dd3e44
Sameer K Muckatira <smuckati@mathworks.com>
2024-02-28 15:22:51 -0800

    Updated project to include generation of an app

--- Utilities/scripts_CIAutomation/genSimpleApp.m
+++ Utilities/scripts_CIAutomation/genSimpleApp.m
@@ -2,31 +2,19 @@ function genSimpleApp()
 % This script is to automate the webview generation of models contained in this project
 
 % List names of all models for which code is being generated
-    mdlName = {'ssc_dcmtr_sl'};
+    appName = {'dcMtr_BehaviorApp'};
 
 % Get handle to project
     prj = matlab.project.currentProject;
     disp(' ')
     disp("Project: " + prj.Name)
 
-% Generate Webview
-    for i = 1:length(mdlName)
-        disp(['Generating WebApp of ' mdlName{i} '...'])
+% Generate app
+    for i = 1:length(appName)
+        disp(['Generating App of ' appName{i} '...'])
 
-        mkdir(mdlName{i})
-        cd(mdlName{i})
-
-        load_system(mdlName{i})
-        simulink.compiler.genapp(mdlName{i}, 'AppName', ['App_' mdlName{i}])
-        close_system(mdlName{i})
-
-        h_fig = findall(0,'Name','Simulation App: ssc_dcmtr_sl');  % Where <App_Title> is the string displayed at the top of the app window.
-        delete(h_fig)
-
-        cd ..
-
-        movefile(mdlName{i},...
-            fullfile(prj.RootFolder,'GeneratedArtifacts','DeployableApps'))
+        mcc('-m',[appName{i} '.mlapp'], ...
+            '-d',fullfile(prj.RootFolder,'GeneratedArtifacts','DeployableApps',appName{i}))
     end
 
 % Cleanup
