120753f66e067e8d46ff701dba967d06da129542
sameerkariappa <smuckati@mathworks.com>
2024-03-25 13:52:12 -0700

    Updated App Generation for mcc to call out to the full app file location

--- Utilities/scripts_CIAutomation/genSimpleApp.m
+++ Utilities/scripts_CIAutomation/genSimpleApp.m
@@ -2,7 +2,7 @@ function genSimpleApp()
 % This script is to automate the webview generation of models contained in this project
 
 % List names of all models for which code is being generated
-    appName = {'dcMtr_BehaviorApp'};
+    appName = {'dcMtr_BehaviorApp.mlapp'};
 
 % Get handle to project
     prj = matlab.project.currentProject;
@@ -12,8 +12,10 @@ function genSimpleApp()
 % Generate app
     for i = 1:length(appName)
         disp(['Generating App of ' appName{i} '...'])
+        
+        appLocation = which(appName{i});
 
-        mcc('-m',[appName{i} '.mlapp'], ...
+        mcc('-m',appLocation, ...
             '-d',fullfile(prj.RootFolder,'GeneratedArtifacts','DeployableApps',appName{i}))
     end
 
