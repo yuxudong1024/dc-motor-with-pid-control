31cc48ace14ddb032dc32e889455bf15c9beed96
sameerkariappa <smuckati@mathworks.com>
2024-06-27 14:48:58 -0700

    Updated app building to use compiler.build instead of mcc

--- Utilities/scripts_CIAutomation/genSimpleApp.m
+++ Utilities/scripts_CIAutomation/genSimpleApp.m
@@ -2,7 +2,7 @@ function genSimpleApp()
 % This script is to automate the webview generation of models contained in this project
 
 % List names of all models for which code is being generated
-    appName = {'dcMtr_BehaviorApp.mlapp'};
+    appName = {'dcMtr_BehaviorApp'};
 
 % Get handle to project
     prj = matlab.project.currentProject;
@@ -13,10 +13,22 @@ function genSimpleApp()
     for i = 1:length(appName)
         disp(['Generating App of ' appName{i} '...'])
         
-        appLocation = which(appName{i});
+        % appLocation = which(appName{i});
 
-        mcc('-m',appLocation, ...
-            '-d',fullfile(prj.RootFolder,'GeneratedArtifacts','DeployableApps',appName{i}))
+        % mcc('-m',appLocation, ...
+        %    '-d',fullfile(prj.RootFolder,'GeneratedArtifacts','DeployableApps',appName{i}))
+
+    % Create target build options object, set build properties and build.
+        buildOpts = compiler.build.StandaloneApplicationOptions(fullfile(prj.RootFolder,"Analysis App", [appName+".mlapp"]));
+        buildOpts.AutoDetectDataFiles = true;
+        buildOpts.OutputDir = fullfile(prj.RootFolder,'GeneratedArtifacts','DeployableApps',appName{i});
+        buildOpts.Verbose = true;
+        buildOpts.EmbedArchive = true;
+        buildOpts.ExecutableName = appName{i};
+        buildOpts.ExecutableVersion = "1.0";
+        buildOpts.TreatInputsAsNumeric = false;
+        buildResult = compiler.build.standaloneApplication(buildOpts);
+
     end
 
 % Cleanup
