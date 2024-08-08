debadce527401e9488a02ce02ce657f78b230d62
sameerkariappa <smuckati@mathworks.com>
2024-08-08 10:08:50 -0700

    Moved BuildTool Work to Independent branch, plan to focus on PAdv on master

--- updategithub.m
+++
@@ -1,6 +0,0 @@
-%% uncomment this line if you want to set the upstream up
-
-%system('git remote add upstream https://github.com/yuxudong1024/dc-motor-with-pid-control')
-
-system('git push upstream master')
-system('git push --tags upstream')
